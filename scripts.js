aac0f18b45d0d881a650a5170e9b42c00e12e837
MidianEden <aearthaternos@gmail.com>
2025-05-02 18:00:37 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Example of a smooth scroll function for anchor links
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
